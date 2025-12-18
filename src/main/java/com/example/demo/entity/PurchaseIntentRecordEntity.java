76aed460af463eb37e9149c4b3326e7eb611b495
bala-amypo <bala@amypo.in>
2025-12-18 14:37:26 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/PurchaseIntentRecordEntity.java
@@ -0,0 +1,68 @@
+package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class PurchaseIntentRecordEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private Double amount;
+    private String category;
+    private String merchant;
+    private LocalDateTime IntentDate;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getUserId() {
+        return userId;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    public Double getAmount() {
+        return amount;
+    }
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public String getMerchant() {
+        return merchant;
+    }
+    public void setMerchant(String merchant) {
+        this.merchant = merchant;
+    }
+    public LocalDateTime getIntentDate() {
+        return IntentDate;
+    }
+    public void setIntentDate(LocalDateTime intentDate) {
+        IntentDate = intentDate;
+    }
+    public PurchaseIntentRecordEntity() {
+    }
+    public PurchaseIntentRecordEntity(Long id, Long userId, Double amount, String category, String merchant,
+            LocalDateTime intentDate) {
+        this.id = id;
+        this.userId = userId;
+        this.amount = amount;
+        this.category = category;
+        this.merchant = merchant;
+        IntentDate = intentDate;
+    }
+    
+}
