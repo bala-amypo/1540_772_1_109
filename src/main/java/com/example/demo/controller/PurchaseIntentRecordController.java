2e27c74c0be90831b4f9a452a259c7cf641e8327
bala-amypo <bala@amypo.in>
2025-12-18 14:38:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/PurchaseIntentRecordController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.PurchaseIntentRecordEntity;
+import com.example.demo.service.PurchaseIntentRecordService;
+
+@RestController
+public class PurchaseIntentRecordController {
+    @Autowired
+    PurchaseIntentRecordService ps;
+    @PostMapping("/rewardrule")
+    public PurchaseIntentRecordEntity create(@RequestBody PurchaseIntentRecordEntity pe){
+        return ps.createIntent(pe);
+    }
+    @GetMapping("/user/{userId}")
+    public Optional<PurchaseIntentRecordEntity> get(@PathVariable Long re){
+        return ps.getIntentByUser(re);
+    }
+    @GetMapping("/Id/{id}")
+    public Optional<PurchaseIntentRecordEntity> getid(@PathVariable Long re){
+        return ps.getIntentById(re);
+    }
+    @GetMapping("/all")
+    public List<PurchaseIntentRecordEntity> getall(){
+        return ps.getAllIntents();
+    }
+}
---
+++ src/main/java/com/example/demo/service/PurchaseIntentRecordServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.PurchaseIntentRecordEntity;
+import com.example.demo.repository.PurchaseIntentRecordRepository;
+
+@Service
+public class PurchaseIntentRecordServiceImpl implements PurchaseIntentRecordService{
+    @Autowired
+    PurchaseIntentRecordRepository pr;
+    @Override
+    public PurchaseIntentRecordEntity createIntent(PurchaseIntentRecordEntity pe){
+        return pr.save(pe);
+    }
+    @Override
+    public Optional<PurchaseIntentRecordEntity> getIntentByUser(Long userId){
+        return pr.findById(userId);
+    }
+    @Override
+    public Optional<PurchaseIntentRecordEntity> getIntentById(Long id){
+        return pr.findById(id);
+    }
+    @Override
+    public List<PurchaseIntentRecordEntity> getAllIntents(){
+        return pr.findAll();
+    }
+}
