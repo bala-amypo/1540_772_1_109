cdf365a20f2cff2be0beebdff656de68ea5b6fe1
bala-amypo <bala@amypo.in>
2025-12-22 05:46:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserProfile.java
+++ src/main/java/com/example/demo/entity/UserProfile.java
@@ -66,10 +66,13 @@ public class UserProfile {
 
 
     public Long getId() {
-         return id; }
-    public void setId(Long id) { this.id = id; }
+         return id; 
+         }
+    public void setId(Long id) {
+     this.id = id; 
+     }
 
-    public String getUserId() { return userId; }
+    public String getUserId(){return userId;}
     public void setUserId(String userId) { this.userId = userId; }
 
     public String getFullName() { return fullName; }
