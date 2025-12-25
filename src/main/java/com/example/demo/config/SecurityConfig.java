64bbf282e91e3e42b4abe4ecae044bb77057cf7a
bala-amypo <bala@amypo.in>
2025-12-25 07:04:16 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,3 +1,19 @@
+package com.example.demo.config;
+
+import com.example.demo.security.JwtAuthenticationFilter;
+import com.example.demo.security.JwtUtil;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
